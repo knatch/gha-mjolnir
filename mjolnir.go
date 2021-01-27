978ffcae9d22a03978b09a29e7e97921df149ac8
Natch Khongpasuk <natch.kh@gmail.com>
2021-01-27 01:12:19 -0500

    log crossRepoIssues return

--- mjolnir.go
+++ mjolnir.go
@@ -56,6 +56,7 @@ func closeRelatedIssues(ctx context.Context, client *github.Client, owner string
 	}
 
 	crossRepoIssues := parseCrossRepoIssueFixes(pr.GetBody())
+	log.Printf("crossRepoIssues: %v", crossRepoIssues)
 
 	return nil
 }
